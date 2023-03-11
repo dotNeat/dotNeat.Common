f31a2f75b433987c9fec41f41a6d4e30661045f4
Andrey Kornich <akornich@gmail.com>
2023-03-10 17:20:14 -0800

    updated the EventsPattern

--- dotNeat.Common/dotNeat.Common.Patterns/EventsPattern/ObjectEventingExtensions.cs
+++ dotNeat.Common/dotNeat.Common.Patterns/EventsPattern/ObjectEventingExtensions.cs
@@ -1,14 +1,10 @@
-﻿namespace dotNeat.Common.Patterns.EventsPattern
-{
-    using System;
+﻿using System;
 
+namespace dotNeat.Common.Patterns.EventsPattern
+{
     public static class ObjectEventingExtensions
     {
-        public static void RaiseDataChangeEventHandler<T>(
-            this object obj,
-            EventHandler<DataChangeEventArgs<T>>? eventHandler,
-            DataChangeEventArgs<T> eventArgs
-            )
+        public static void RaiseDataChangeEventHandler<T>(this object? obj, EventHandler<DataChangeEventArgs<T>>? eventHandler, DataChangeEventArgs<T> eventArgs)
         {
             EventHandler<DataChangeEventArgs<T>>? eh = eventHandler;
             if (eh != null)
